8536208e2786f458482d8084b98d359c33adaf37
Syed Hamza Aleem <162742428+Hamzaaleem230@users.noreply.github.com>
2025-09-05 16:27:24 +0500

    Heading.tsx

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -7,11 +7,11 @@ interface PropsType {
 const Heading: React.FC<PropsType> = ({ title }) => {
   return (
     <div className="text-center pb-8">
-      <p className="text-2xl sm:text-3xl lg:text-4xl font-bold inline-block border-b-4 border-primary pb-2 text-gray-900 dark:text-gray-100 transition duration-300 hover:scale-105">
+      <p className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold text-primary transition duration-300 hover:scale-105">
         {title}
       </p>
     </div>
   );
 };
 
-export default Heading;
+export default Heading;

ace36208150b28c489b42449e4917b94be69c0c4
Syed Hamza Aleem <162742428+Hamzaaleem230@users.noreply.github.com>
2025-09-05 21:18:16 +0500

    Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,12 +5,13 @@ const Footer = () => {
     <footer className="py-6 text-center border-t border-gray-700 dark:bg-gray-900 dark:text-gray-300 text-sm sm:text-base font-medium">
       <p>
         © 2025 All Rights Reserved. | Built with ❤️ by{" "}
-        <span className="text-[#d1d5db]">Syed Hamza Aleem</span>
+        <span className="dark:text-gray-300">Syed Hamza Aleem</span>
       </p>
     </footer>
   );
 };
 
 export default Footer;
+
 
 

2cc6e19af2ceb95cc3d794dd5613361396ec118a
Syed Hamza Aleem <162742428+Hamzaaleem230@users.noreply.github.com>
2025-09-10 01:09:23 +0500

    Add files via upload

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -7,11 +7,8 @@ export async function POST(req: NextRequest) {
   try {
     const { messages } = await req.json();
 
-  
-    const userMsg = messages?.[messages.length - 1]?.content ?? "";
-
     const systemPrompt = `
-Tum Hamza Aleemke portfolio ka AI assistant ho.
+Tum Syed Hamza Aleem ke portfolio ka AI assistant ho.
 - Name: ${profile.name}
 - Role: ${profile.role}
 - Skills: ${profile.skills.join(", ")}
@@ -24,6 +21,8 @@ Rules:
 1) Roman English me jawab do, friendly & professional.
 2) Sirf di gayi info par based ho. Agar data nahi to bolo: "Ye info mere paas nahi."
 `;
+
+    const userMsg = messages?.[messages.length - 1]?.content ?? "";
 
     const res = await fetch(
       `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
@@ -52,7 +51,7 @@ Rules:
       headers: { "Content-Type": "application/json" },
     });
   } catch (err) {
-    console.error(err);
+    console.log(err);
     return new Response(JSON.stringify({ error: "Server error" }), {
       status: 500,
     });
